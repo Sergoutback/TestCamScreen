9a0e560788798e94e9cf93525129f43d0cfa1d8d
SergoutbackDesktop <webart2013@gmail.com>
2021-09-09 18:54:36 +0300

    09092021_1854
    can't fix screenshot

--- Assets/Scripts/PhotoCapture.cs
+++ Assets/Scripts/PhotoCapture.cs
@@ -14,13 +14,18 @@ public class PhotoCapture : MonoBehaviour
         int width = this.captureCamera.pixelWidth;
         int height = this.captureCamera.pixelHeight;
         Texture2D texture = new Texture2D(width, height);
+
         RenderTexture targetTexture = RenderTexture.GetTemporary(width, height);
+
         this.captureCamera.targetTexture = targetTexture;
         this.captureCamera.Render();
+
         RenderTexture.active = targetTexture;
+
         Rect rect = new Rect(0, 0, width, height);
         texture.ReadPixels(rect, 0, 0);
-        texture.Apply();   
+        texture.Apply();  
+        
         displayscreen.texture = targetTexture;
         Debug.Log("MakeScrenshot()");    }
 }
